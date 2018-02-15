97b034703aa3050f4e18b5acd114386b1c8dcd0c
Guest User <Guest@epicodus-97.local>
2018-02-15 10:12:35 -0800

    dry up code

--- Palindrome/Models/Palindrome.cs
+++ Palindrome/Models/Palindrome.cs
@@ -10,30 +10,33 @@ namespace PalindromeProject.Models
     {
 
       string userWord = word;
-      // string userWord = "random";
+      // string userWord = "word";
 
       Stack<char> reversedWord = new Stack<char>();
 
-      Stack<char> normalizedWord = new Stack<char>();
-
-      Stack<char> tempReversed = new Stack<char>();
+	    string result = "";
 
       foreach(char character in userWord)
       {
         reversedWord.Push(character);
-        tempReversed.Push(character);
       }
+
+      //reversedWord = {"d" "o" "r" "w"}
 
       for(int i = 0; i < userWord.Length; i++)
       {
         char extractedChar = reversedWord.Pop();
-        normalizedWord.Push(extractedChar);
+        //pops d
+        //pops o
+		    result += extractedChar;
+        //result = "d"
+        //result = "d" + "o"
+        //result = "do" + "r"
+        //result = "dor" + "w"
+        //result = "dorw"
       }
 
-      string strTempReversed = tempReversed.ToString();
-      string strNormalizedWord = normalizedWord.ToString();
-
-      if(strTempReversed == strNormalizedWord)
+      if(result.Equals(userWord))
       {
         return true;
       }
@@ -41,6 +44,7 @@ namespace PalindromeProject.Models
       {
         return false;
       }
+
 
     }
   }
